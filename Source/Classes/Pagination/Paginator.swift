d4c650d2a4beec802d9e4acb391188ac77868a85
Ravindra Soni <soni@nickelfox.com>
2017-09-15 11:53:18 +0530

    refactor

--- Source/Classes/Pagination/Paginator.swift
+++ Source/Classes/Pagination/Paginator.swift
@@ -12,7 +12,7 @@ import JSONParsing
 public let defaultPageSize: Int = 20
 
 public protocol Pageable: JSONParseable {
-	static func fetch(request: PageRequest, completion: @escaping (_ result: APIResult<ListResponse<Self>>) -> Void)
+	static func fetch(router: Router, completion: @escaping (_ result: APIResult<[Self]>) -> Void)
 }
 
 public class Paginator<T: Pageable> {
@@ -30,14 +30,14 @@ public class Paginator<T: Pageable> {
 	
 	public func loadNextPage(completion:  @escaping (_ result: APIResult<[T]>) -> Void) {
 		let request = PageRequest(router: self.router, index: self.currentIndex, limit: self.limit)
-		T.fetch(request: request) { [weak self] (result: APIResult<ListResponse<T>>) in
+		T.fetch(router: request) { [weak self] (result: APIResult<[T]>) in
 			guard let this = self else { return }
 			switch result {
-			case .success(let listResponse):
-				for item in listResponse.list {
+			case .success(let items):
+				for item in items {
 					this.items.append(item)
 				}
-				this.canLoadMore = listResponse.list.count >= this.limit
+				this.canLoadMore = items.count >= this.limit
 				if this.canLoadMore {
 					this.currentIndex += 1
 				}
@@ -51,4 +51,30 @@ public class Paginator<T: Pageable> {
 		self.items.removeAll()
 		self.loadNextPage(completion: completion)
 	}
+}
+
+struct PageRequest: Router {
+	
+	let router: Router
+	let index: Int
+	let limit: Int
+	
+	var params: [String: Any] {
+		return self.router.compiledParams(index: self.index, limit: self.limit)
+	}
+	
+	var method: HTTPMethod { return self.router.method }
+	
+	var keypathToMap: String? { return self.router.keypathToMap }
+	
+	var headers: [String : String]  { return self.router.headers }
+	
+	var baseUrl: URL { return self.router.baseUrl }
+	
+	var path: String { return self.router.path }
+	
+	var timeoutInterval: TimeInterval? { return router.timeoutInterval }
+	
+	var encoding: URLEncoding? { return self.router.encoding }
+	
 }
