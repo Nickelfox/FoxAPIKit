50532deaa9ebd6d97493a2565d66249c7ba7d182
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-09 21:58:08 +0530

    add: framework for macOS, tvOS

---
+++ APIClient tvOS/APIClient_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  APIClient_tvOS.h
+//  APIClient tvOS
+//
+//  Created by Vaibhav Parmar on 09/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIClient_tvOS.
+FOUNDATION_EXPORT double APIClient_tvOSVersionNumber;
+
+//! Project version string for APIClient_tvOS.
+FOUNDATION_EXPORT const unsigned char APIClient_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIClient_tvOS/PublicHeader.h>
+
+
