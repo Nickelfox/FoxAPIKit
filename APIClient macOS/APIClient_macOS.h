d885989db7312d861cf0614e5eb4c1e8f98fab1e
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-10 16:37:38 +0530

    add: carthage support

---
+++ APIClient macOS/APIClient_macOS.h
@@ -0,0 +1,19 @@
+//
+//  APIClient_macOS.h
+//  APIClient macOS
+//
+//  Created by Vaibhav Parmar on 10/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for APIClient_macOS.
+FOUNDATION_EXPORT double APIClient_macOSVersionNumber;
+
+//! Project version string for APIClient_macOS.
+FOUNDATION_EXPORT const unsigned char APIClient_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIClient_macOS/PublicHeader.h>
+
+
