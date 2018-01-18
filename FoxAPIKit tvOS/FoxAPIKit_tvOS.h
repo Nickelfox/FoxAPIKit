85bee5fabc873a1ab8be06368466d8025cd3223e
Ravindra Soni <soni@nickelfox.com>
2018-01-18 17:05:30 +0530

    chore: project inconsistenciew

---
+++ FoxAPIKit tvOS/FoxAPIKit_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  FoxAPIKit_tvOS.h
+//  FoxAPIKit tvOS
+//
+//  Created by Ravindra Soni on 18/01/18.
+//  Copyright © 2018 Ravindra Soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoxAPIKit_tvOS.
+FOUNDATION_EXPORT double FoxAPIKit_tvOSVersionNumber;
+
+//! Project version string for FoxAPIKit_tvOS.
+FOUNDATION_EXPORT const unsigned char FoxAPIKit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoxAPIKit_tvOS/PublicHeader.h>
+
+
---
+++ FoxAPIKit/FoxAPIKit.h
@@ -0,0 +1,19 @@
+//
+//  FoxAPIKit.h
+//  FoxAPIKit
+//
+//  Created by Ravindra Soni on 18/01/18.
+//  Copyright © 2018 Ravindra Soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoxAPIKit.
+FOUNDATION_EXPORT double FoxAPIKitVersionNumber;
+
+//! Project version string for FoxAPIKit.
+FOUNDATION_EXPORT const unsigned char FoxAPIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoxAPIKit/PublicHeader.h>
+
+
