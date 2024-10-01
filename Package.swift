24c7c1cd826ebefbe0be0056abe4304c2b71441c
Vaibhav Parmar <vaibhav@nickelfox.com>
2024-10-01 16:20:05 +0530

    chore(dependencies): update min deployment target

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "FoxAPIKit",
     platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v3)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
